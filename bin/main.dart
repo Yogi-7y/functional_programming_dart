952b8a4db9753f266692474fcfffd37b4555de96
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2022-03-05 14:03:16 +0530

    Imperative vs Declarative

---
+++ bin/main.dart
@@ -0,0 +1,9 @@
+/// Make sure to run the main method in debug mode as asserts are not validated in release mode.
+/// If running through command line, pass the --enable-asserts flag.
+import 'package:functional_programming_dart/0_foundation/foundation.dart';
+
+void main(List<String> args) {
+  /// Part 0. Foundation
+  filterEvensInImperativeStyle();
+  filterEvensInDeclarativeStyle();
+}
---
+++ lib/0_foundation/foundation.dart
@@ -0,0 +1,35 @@
+// Demonstrate imperative vs declarative programming by filtering out all the even numbers.
+
+import 'package:collection/collection.dart';
+
+const _numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const _expectedResult = [2, 4, 6, 8, 10];
+
+/// Imperative approach
+void filterEvensInImperativeStyle() {
+  final _result = <int>[];
+
+  for (int i in _numbers) {
+    if (i % 2 == 0) {
+      _result.add(i);
+    }
+  }
+
+  assert(
+    const ListEquality().equals(_result, _expectedResult),
+    "Imperative approach - Expected: $_expectedResult, Actual: $_result",
+  );
+  print('Imperative Evens ✅');
+}
+
+/// Declarative approach
+void filterEvensInDeclarativeStyle() {
+  final _result = _numbers.where((element) => element % 2 == 0).toList();
+
+  assert(
+    const ListEquality().equals(_result, _expectedResult),
+    "Declarative approach - Expected: $_expectedResult, Actual: $_result",
+  );
+
+  print('Declarative Evens ✅');
+}
