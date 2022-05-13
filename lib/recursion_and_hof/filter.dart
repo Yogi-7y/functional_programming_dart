e64e7439a59544aff8589b423016101913813874
Yogesh Parwani <yogeshparwani99.yp@gmail.com>
2022-05-13 07:47:41 +0530

    Added imperative approach for map and filter

--- lib/recursion_and_hof/filter.dart
+++ lib/recursion_and_hof/filter.dart
@@ -5,6 +5,20 @@ import '../utilities/utilities.dart';
 const _arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 void main(List<String> args) {
+  // Imperative approach
+
+  final _result = <int>[];
+
+  for (final element in _arr) {
+    if (element.isEven) {
+      _result.add(element);
+    }
+  }
+
+  print(_result); // [2, 4, 6, 8, 10]
+
+  // Declarative approach
+
   final _evens = filter<int>(isEven, _arr);
   print(_evens); // [2, 4, 6, 8, 10]
 
--- lib/recursion_and_hof/map.dart
+++ lib/recursion_and_hof/map.dart
@@ -3,6 +3,18 @@ import '../utilities/utilities.dart';
 const _arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 void main(List<String> args) {
+  // Imperative approach
+
+  final _result = <int>[];
+
+  for (final element in _arr) {
+    _result.add(element * 2);
+  }
+
+  print(_result); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+  // Declarative approach
+
   final _doubles = map<int>(doubleIt, _arr);
   print(_doubles); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 }
